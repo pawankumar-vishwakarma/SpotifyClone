8e112c99c50ff243aee10468eb290415e0df94a8
Pawan Kumar <pawan07052001@gmail.com>
2023-09-07 19:11:34 +0530

    worked on play and pause button and updated song list in html through an array(song image, songName, duration).

--- js/script.js
+++ js/script.js
@@ -1,2 +1,52 @@
-let audioElement = new Audio('/songs/Shriman Narayan Hari Hari(PagalWorld.cm).mp3');
-//audioElement.play();
+//Initialize the variables
+let songIndex = 0;
+let audioElement = new Audio('/songs/Shriman Narayan Hari Hari.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let songItem = Array.from(document.getElementsByClassName('songItem'));
+let songs = [
+    {songName:"Shriman Narayan Hari Hari", filePath: "/songs/Shriman Narayan Hari Hari.mp3", coverPath: "/img/shreeman.jpg", timeStamp: "04:29"},
+    {songName:"Tumhe Kitna Pyaar Karte - Bawaal", filePath: "/songs/Tumhe Kitna Pyaar Karte - Bawaal.mp3", coverPath: "/img/Tumhe Kitna Pyaar Karte.jpg", timeStamp: "05:05"},
+    {songName:"Ve Kamleya - Rocky Aur Rani Kii Prem Kahaani", filePath: "/songs/Ve Kamleya - Rocky Aur Rani Kii Prem Kahaani.mp3", coverPath: "/img/Ve Kamleya.jpg", timeStamp: "04:07"},
+    {songName:"Yaar Ka Sataya Hua Hai - B Praak", filePath: "/songs/Yaar Ka Sataya Hua Hai - B Praak.mp3", coverPath: "/img/Yaar Ka Sataya Hua Hai.jpg", timeStamp: "04:27"},
+    {songName:"Shriman Narayan Hari Hari", filePath: "/songs/Shriman Narayan Hari Hari.mp3", coverPath: "/img/shreeman.jpg", timeStamp: "04:29"},
+    {songName:"Tumhe Kitna Pyaar Karte - Bawaal", filePath: "/songs/Tumhe Kitna Pyaar Karte - Bawaal.mp3", coverPath: "/img/Tumhe Kitna Pyaar Karte.jpg", timeStamp: "05:05"},
+    {songName:"Ve Kamleya - Rocky Aur Rani Kii Prem Kahaani", filePath: "/songs/Ve Kamleya - Rocky Aur Rani Kii Prem Kahaani.mp3", coverPath: "/img/Ve Kamleya.jpg", timeStamp: "04:07"},
+    {songName:"Yaar Ka Sataya Hua Hai - B Praak", filePath: "/songs/Yaar Ka Sataya Hua Hai - B Praak.mp3", coverPath: "/img/Yaar Ka Sataya Hua Hai.jpg", timeStamp: "04:27"},
+    {songName:"Shriman Narayan Hari Hari", filePath: "/songs/Shriman Narayan Hari Hari.mp3", coverPath: "/img/shreeman.jpg", timeStamp: "04:29"},    
+    {songName:"Shriman Narayan Hari Hari", filePath: "/songs/Shriman Narayan Hari Hari.mp3", coverPath: "/img/shreeman.jpg", timeStamp: "04:29"}
+]
+
+songItem.forEach((element, i) => {
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+    element.getElementsByClassName("duration")[0].innerText = songs[i].timeStamp;
+});
+
+//Handle play/pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        masterPlay.title = "Pause";
+        gif.style.opacity = 1;
+    }else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        masterPlay.title = "Play";
+        gif.style.opacity = 0;
+    }
+});
+//Listen to Events
+audioElement.addEventListener('timeupdate',()=>{
+    //update seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100);
+    myProgressBar.value = progress;
+});
+
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime = (myProgressBar.value * audioElement.duration)/ 100;
+});
